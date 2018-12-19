71f4e6d791aa15ddd930a56e8abdf213446c63b3
Anatoly Kireev <anatoly@kopikot.ru>
2018-12-19 15:32:36 +0300

    Get connection options from command line argument

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log('123');
+import commander from 'commander';
+
+import { ICredentials } from './interfaces';
+import { parseConnectionArg } from './utils';
+
+commander
+  .version('1.0.0')
+  .command('* [user:password@hostname]')
+  .description('connect to ssh server')
+  .action((args) => {
+    // const arg = 'root:pxtm0222@10.8.0.22';
+    const connectionConfig = parseConnectionArg(args);
+    const session = createSession(connectionConfig);
+  });
+
+function createSession (config: ICredentials) {
+  // tslint:disable-next-line:no-console
+  console.log(`Connecting to ${config.host}...`);
+}
+
+commander.parse(process.argv);
