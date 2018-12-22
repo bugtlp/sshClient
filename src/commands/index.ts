c10853b9abfc085b77345fcd821534daf956dfd5
Anatoly Kireev <anatoly@kopikot.ru>
2018-12-22 03:14:36 +0300

    Add command for get file

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,12 +1,29 @@
-import { Client } from 'ssh2';
+import path from 'path';
+import { Client, SFTPWrapper } from 'ssh2';
+import { promisify } from 'util';
+import { print } from '../utils';
 
 class Command {
   constructor (private connection: Client) {}
 
-  async execute (...args: string[]) {
-    // tslint:disable-next-line:no-console
-    console.log('command execute with params', args);
-    await 'completed!!!\n';
+  async download (sftp: SFTPWrapper, remotePath: string, localPath: string) {
+    print(`Downloading from remoteHost:${remotePath} to 127.0.0.1:${localPath}`);
+    const fastGetAsync = promisify<string, string>(sftp.fastGet.bind(sftp));
+    try {
+      await fastGetAsync(remotePath, localPath);
+      print('File is downloaded successfully');
+    } finally {
+      sftp.end();
+    }
+  }
+
+  async execute (...args: string[]): Promise<void> {
+    const remotePath = args[0];
+    const fileName = path.basename(remotePath);
+    const localPath = path.join(process.cwd(), fileName);
+    const requestSFTP = promisify(this.connection.sftp.bind(this.connection));
+    const sftp = await requestSFTP();
+    return this.download(sftp, remotePath, localPath);
   }
 }
 
